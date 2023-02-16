f280f16811195be97f0526b7102cb673f41e30d3
brn-42 <bruno.mlt@cora.com.br>
2023-02-16 12:18:58 -0300

    migrations

---
+++ forum/src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,40 @@
+
+create table course(
+    id serial not null,
+    name varchar(50) not null,
+    category varchar(50) not null,
+    primary key(id)
+);
+
+create table forum_user(
+    id serial not null,
+    name varchar(50) not null,
+    email varchar(50) not null,
+    primary key(id)
+);
+
+create table topic(
+    id serial not null,
+    title varchar(50) not null,
+    message varchar(255) not null,
+    created_at timestamp not null,
+    status varchar(25) not null,
+    course_id bigint not null,
+    author_id bigint not null,
+
+    primary key(id),
+    foreign key(course_id) references course(id),
+    foreign key(author_id) references forum_user(id)
+);
+
+create table response(
+    id serial not null,
+    message varchar(255) not null,
+    created_at timestamp not null,
+    author_id bigint not null,
+    topic_id bigint not null,
+    resolved boolean not null,
+
+    foreign key(topic_id) references topic(id),
+    foreign key(author_id) references forum_user(id)
+);
